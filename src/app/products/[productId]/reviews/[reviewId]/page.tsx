1e1654244833da71d37fa598f0409dd812848dc2
omarelnahs <omarelnahas1141@gmail.com>
2024-08-09 06:46:03 +0200

    error file

--- src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,4 +1,9 @@
 import {notFound} from "next/navigation"; 
+
+function getRandomInt (count: number){
+  return Math.floor(Math.random()*count)
+}
+
 export default function ReviewsDetails({
   params,
 }: {
@@ -7,6 +12,12 @@ export default function ReviewsDetails({
     reviewId: string ;
 };
 }) {
+  const random = getRandomInt(2)
+
+  if(random === 1){
+    throw new Error("Error loading review")
+  }
+
   if(parseInt(params.reviewId)>1000){
     notFound();
   }
